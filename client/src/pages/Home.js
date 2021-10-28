ea14301d32a7b9cb80806c93e811aa022a362737
Susma Gajmer <gajmersusma@gmail.com>
2021-10-28 16:10:54 -0500

    HTML code and css code for the Homepage
    Homepage and css file

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,28 @@
-import React from 'react';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Commerce Bank</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+</head>
+<body>
 
-export default function Home() {
+    <div id="logo">
+	<img src="logo.png">
+i></a></li> <!-- Brandon can link his login page here-->
+</div>
 
-    return (
-        <div>
-            Poopy Head
-        </div>
-    )
-}
+  <div class="wrapper">
+    <div class="center">
+      <h1>Welcome to Commerce Online Banking </h1>
+    </div>
+
+  </div>
+  <footer> &copy; Copyright © 2021 Commerce Bancshares, Inc. All rights reserved. Susma & the boys</footer>
+
+
+</body>
+</html>
