5ae6628980f214bf065e19e61eb7cd6e3f2cfc1a
Brandon Olson <bmolson14@gmail.com>
2021-11-03 17:34:16 -0500

    Adding home.css and home.js edits

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Home.css'
 
 export default function Home() {
+    
 
     return (
-        <body>
-            Helooooo
-        </body>
+            
+        <div className="home-c">
+            <div className ="sub">
+            <form onSubmit>
+                <input type='username' name='username' placeholder='email...' required onChange/>
+                <input type='password' name='password' placeholder='password...' required onChange/>
+                <button onSubmit>Log In</button>
+            </form>
+            </div>
+        </div>
     )
+    
 }
