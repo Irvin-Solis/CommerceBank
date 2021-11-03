389f718b5880b178acd78dedb15b1bec0ca1dda5
Daniel <hotrode19@gmail.com>
2021-11-03 18:41:06 -0500

    table

--- client/src/pages/Transactions.js
+++ client/src/pages/Transactions.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import MUIDataTable from "mui-datatables";
 
 const Transactions = (props) => {
 
+    const options = {
+        filter: false,
+        selectableRows: "none",
+        print: false,
+        download: false,
+        expandableRows: false,
+        rowHover:true,
+        pagination:true,
+        search:false,
+        sortOrder: {name: "Name", direction: "asc"}
+    };
+    
+    let col = ["trans num", "date", "ammount", "balance", "description"];
+
+    let Tabledata = [["1", "2019-11-04", "", "3000", "deposit"], ["2", "2019-11-08", "45", "3955", "Walmart"]]
+
     return (
         <div>
-            Hello There
+            <MUIDataTable
+            title={"Transactions"}
+            data={Tabledata}
+            columns={col}
+            options={options}
+          />
         </div>
     )
 }
