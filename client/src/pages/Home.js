f8560d4f154eabfc853e8d071c3adb7f0241f542
PerimoOmnes <69551268+PerimoOmnes@users.noreply.github.com>
2021-11-03 19:15:35 -0400

    Reset home (#12)
    * Create Home.css
    
    * added the correct css
    
    * finish pull
    
    Co-authored-by: Brandon Olson <bmolson14@gmail.com>

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Home.css'
 
 export default function Home() {
+    
 
     return (
-        <body>
-            Helooooo
-        </body>
+            
+        <div className="home-c">
+            <div className ="sub">
+
+            <form onSubmit>
+                <input type='username' name='username' placeholder='email...' required onChange/>
+                <input type='password' name='password' placeholder='password...' required onChange/>
+                <div className="profile">
+                    <button onSubmit>Log In</button>
+                </div>
+                <p className="link">
+                    <a href="#">Forgot password?</a> Or<a href="#">Sign Up</a>
+                </p>
+            </form>
+            </div>
+        </div>
     )
+    
 }
