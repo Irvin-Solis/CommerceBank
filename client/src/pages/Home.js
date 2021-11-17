038cd4855f10e8b25a833ccc66c377f21eb6ab1a
Brandon Olson <bmolson14@gmail.com>
2021-11-17 17:17:47 -0600

    added login without db

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
+import LoginForm from './LoginForm';
 
 export default function Home() {
-    
+    const adminUser = {
+        email: "test@gmail.com",
+        password: "test"
+    }
+    const [user,setUser] = useState({name: "", email: ""});
+    const [error, setError] = useState("");
 
+    const Login = details => {
+        console.log(details);
+
+        if (details.email == adminUser.email && details.password == adminUser.password) {
+            console.log("Logged in");
+            setUser({
+                name:details.name,
+                email: details.email,
+            });
+        } else {
+            console.log("Invalid Login");
+            setError("Invalid Login");
+        }
+    }
+    const Logout = () => {
+        setUser({name: "", email: ""})
+    } 
     return (
             
         <div className="home-c">
-            <div className ="sub">
-
-            <form onSubmit>
-                <input type='username' name='username' placeholder='email...' required onChange/>
-                <input type='password' name='password' placeholder='password...' required onChange/>
-                <div className="profile">
-                    <button onSubmit>Log In</button>
+            {(user.email != "") ? (
+                <div className="welcome">
+                    <h2>Login, <span>{user.name}</span></h2>
+                    <button onClick ={Logout}>Logout</button>
                 </div>
-                <p className="link">
-                    <a href="#">Forgot password?</a> Or<a href="#">Sign Up</a>
-                </p>
-            </form>
-            </div>
+            )   : (
+                <LoginForm Login={Login} error={error} />
+            )}
+            
         </div>
     )
     
